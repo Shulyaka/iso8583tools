492918abfa1d3941a5610a8278ce3112a89acf2d
Denis Shulyaka <Shulyaka@gmail.com>
2013-10-16 00:19:20 +0400

    switch_loadbalancer

---
+++ switch/switch_loadbalancer.c
@@ -0,0 +1,87 @@
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <poll.h>
+#include "isomessage.pb.h"
+
+int main(void)
+{
+	struct sockaddr_un addr;
+	struct pollfd sfd[1];
+	isomessage inmsg;
+
+	char buf[1000];
+	int size;
+
+	GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+	sfd[0].fd=socket(AF_UNIX, SOCK_DGRAM, 0);
+
+	if(sfd[0].fd==-1)
+	{
+		printf("Error: Unable to create a socket: %s\n", strerror(errno));
+		return -1;
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family=AF_UNIX;
+	strncpy(addr.sun_path, "/home/denis/switch", sizeof(addr.sun_path) - 1);
+
+	unlink(addr.sun_path);
+
+	if(bind(sfd[0].fd, (struct sockaddr*)&addr, sizeof(struct sockaddr_un)))
+	{
+		printf("Error: Unable to bind a socket: %s\n", strerror(errno));
+		close(sfd[0].fd);
+		return -1;
+	}
+
+	sfd[0].events=POLLIN;
+
+	while(1)
+	{
+		printf("Waiting for a message...\n");
+
+		if(poll(sfd, 1, -1)==-1)
+		{
+			printf("Error: poll: %s\n", strerror(errno));
+			break;
+		}
+
+		printf("recieving message\n");
+
+		size=recv(sfd[0].fd, buf, sizeof(buf), 0);
+		
+		if(size==-1)
+		{
+			printf("Error: recv: %s\n", strerror(errno));
+			break;
+		}
+
+		if(size==0)
+		{
+			printf("Warning: Client disconnected\n");
+			continue;
+		}
+
+		printf("Size is %d\n", size);
+
+		if(!inmsg.ParseFromArray(buf, size))
+		{
+			printf("Warning: Unable to parse the message\n");
+			continue;
+		}
+
+		printf("\nIncommingMessage:\n");
+		inmsg.PrintDebugString();
+		
+	}
+
+	close(sfd[0].fd);
+
+	google::protobuf::ShutdownProtobufLibrary();
+
+	return 0;
+}
